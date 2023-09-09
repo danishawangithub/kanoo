039be7f72344d0857b81a09a1510d8989b54b34d
smonderoy <143300022+smonderoy@users.noreply.github.com>
2023-09-09 11:44:17 +0500

    arslan

--- routes/api.php
+++ routes/api.php
@@ -26,4 +26,5 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
+    Route::post('to', 'refresh');
 });
